93dd4e76b35a55bdf94af23ba410cf776df8ff91
chudniy <evgeniychudniy@gmail.com>
2017-10-19 00:34:10 +0300

    Install HttpKernel Symfony component. Create class for controller and use controller resolver and argument resolver

--- src/app.php
+++ src/app.php
@@ -8,6 +8,18 @@
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
 
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
+
 function is_leap_year($year = null) {
     if (null === $year) {
         $year = date('Y');
@@ -19,13 +31,7 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => 'LeapYearController::indexAction',
 )));
 
 return $routes;
